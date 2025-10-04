2040f3d01d01ab50eb818b05a817b9db8922a10b
Gaurav Bharat <gaurav.mendse@gmail.com>
2025-10-04 19:56:41 +0530

    Stop tracking data folder

---
+++ ingestion.py
@@ -0,0 +1,16 @@
+import asyncio
+
+
+async def ingest():
+    # 1. Load JSON files data each with json file name and a url based on the file name
+
+    # 2. Split data into separate, semantically meaningful chunks
+
+    # 3. Embed data, create a vector space
+
+    # 4. Store data in vector database
+    pass
+
+
+if __name__ == "__main__":
+    asyncio.run(ingest())
